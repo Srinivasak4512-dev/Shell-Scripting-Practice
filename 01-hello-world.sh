319dba048ebfb283a0080e48e4cbb61a7cee4d3a
Srinivasak4512-dev <ksrini4512@gmail.com>
2025-05-26 06:28:31 +0530

    Shell-Script Practice

---
+++ 01-hello-world.sh
@@ -0,0 +1,3 @@
+#!/bin/bash                 # Shebang line: Tells the system to use the Bash shell to run this script.
+
+echo "Hello World!"         # echo is a built-in Bash command that prints the string "Hello World!" to the terminal.
